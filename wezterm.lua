c50c524458d212bb6ce0cecaf45420a14a620043
lvyonghong <hydex@outlook.com>
2023-09-26 14:03:53 +0800

    scroll+1000000

--- wezterm.lua
+++ wezterm.lua
@@ -10,6 +10,6 @@ local other_opts = {
     check_for_updates = true,
     check_for_updates_interval_seconds = 7 * 24 * 60 * 60,
     use_dead_keys = false,
-    scrollback_lines = 50000
+    scrollback_lines = 1000000
 }
 return utils.merge({ other_opts, startup, keys, ui, title })
