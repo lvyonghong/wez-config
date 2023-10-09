8cb7b5b87134e282f9e4e8b1e656ab104a410fc5
lvyonghong <hydex@outlook.com>
2023-10-09 10:46:33 +0800

    position

--- startup.lua
+++ startup.lua
@@ -6,7 +6,7 @@ wezterm.on('gui-startup', function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
 
     -- window:gui_window():maximize()
-    window:gui_window():set_position(100,50)
+    window:gui_window():set_position(0,0)
     window:gui_window():set_inner_size(3000,2000)
     window:gui_window():perform_action(act.ActivateTab(0), pane)
 end)
